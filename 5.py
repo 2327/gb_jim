bc0a8f8beae4892dc3e871ccf87f058fd28fad66
Maxim Klyuev <klyuevmv@mos.ru>
2019-06-21 17:44:36 +0300

    checkpoint

--- 5.py
+++ 5.py
@@ -3,3 +3,14 @@
 5. Выполнить пинг веб-ресурсов yandex.ru, youtube.com и преобразовать результаты из байтовового в строковый тип на
 кириллице.
 """
+
+import subprocess
+import chardet
+l = ['yandex.ru', 'youtube.com']
+
+ya_ping = subprocess.Popen(args, stdout=subprocess.PIPE)
+for line in ya_ping.stdout:
+    result = chardet.detect(line)
+    print(result)
+    line = line.decode(result['encoding']).encode('utf-8')
+    print(line.decode('utf-8'))
