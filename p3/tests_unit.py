f7864bfbbaddd4c2c62ee72313023370dd42c312
Maxim Klyuev <klyuevmv@mos.ru>
2019-07-02 12:12:09 +0300

    added unittest

---
+++ p3/tests_unit.py
@@ -0,0 +1,17 @@
+import unittest
+from functions import *
+
+class TestTime(unittest.TestCase):
+
+    def test_actual_time(self):
+        self.assertEqual(act_time(int('1561845014')),('30.06.2019 - 00:50:14'))
+
+    def test_convert_dict(self):
+        self.assertIsNone(convert('{"action": "presence", "2": "2"}'))
+
+    def test_convert_empty(self):
+        self.assertIsNone(convert(' '))
+
+
+if __name__ == "__main__":
+    unittest.main()
