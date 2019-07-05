0a797d24e6d41e88c5d9518f700fb8ed69accc52
Maxim Klyuev <klyuevmv@mos.ru>
2019-07-05 13:31:25 +0300

    rename server_log file

--- server/server_log_config.py
+++ server/server_log_config.py
@@ -3,6 +3,9 @@
 '''
 
 import logging
+import time
 
-logging.basicConfig(filename="log/server.log", format="%(asctime)s %(levelname)-10s %(message)s", level=logging.INFO)
+logname = str('log/server-' + time.strftime('%d-%m-%Y') + '.log')
+
+logging.basicConfig(filename = logname, format = "%(asctime)s %(levelname)-10s %(message)s", level = logging.INFO)
 server_log = logging.getLogger('server')
